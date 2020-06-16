593812602bb0cb218b4ade90d212c17200d38e91
kit-smith <csmit356@jhu.edu>
2020-06-15 23:38:29 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## makeCacheMatrix initiates the objects x and i, and then defines the functions 
+## set, get, set.inverse and get.inverse and puts them into a list.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        i<-NULL
+        set<-function(y) {
+                x <<- y
+                i <<- NULL
+         }
+                
+        get <- function() x
+        set.inverse <- function(solve) i <<- solve
+        get.inverse <- function() i
+                
+        list(set = set,
+             get = get,
+             set.inverse = set.inverse,
+             get.inverse = get.inverse)
 }
 
-
-## Write a short comment describing this function
+## If these is a valid inverse cached in i, cacheSolve returns it to the patent
+## environment. If not, then cacheSolve gets x and solves for its inverse.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        i <- x$get.inverse()
+        if(!is.null(i)) {
+                message("getting cached data")
+                return(i)
+        }
+        data<- x$get()
+        i<- solve(data,...)
+        x$set.inverse(i)
+        i
 }
